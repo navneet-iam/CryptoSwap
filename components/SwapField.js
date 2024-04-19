95c813ae862ac785e07af3e0683301a1862d99b7
Navneet <bosshero680@gmail.com>
2024-04-20 01:20:55 +0530

    fix: named component

--- components/SwapField.js
+++ components/SwapField.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Selector from './Selector'
 
-const SwapField = React.forwardRef(({ obj }, inputRef) => {
+const SwapField = ({ obj }, ref) => {
   const { id, value = '', setValue, defaultValue, setToken, ignoreValue } = obj
 
   return (
     <div className='flex items-center rounded-xl'>
       <input
-        ref={inputRef}
-        className={getInputClassname()}
+        ref={ref}
+        className='w-full outline-none h-8 px-2 appearance-none text-3xl bg-transparent'
         type={'number'}
         value={value}
         placeholder={'0.0'}
@@ -25,12 +25,6 @@ const SwapField = React.forwardRef(({ obj }, inputRef) => {
       />
     </div>
   )
+};
 
-  function getInputClassname() {
-    let className =
-      ' w-full outline-none h-8 px-2 appearance-none text-3xl bg-transparent'
-    return className
-  }
-})
-
-export default SwapField
+export default React.forwardRef(SwapField);
